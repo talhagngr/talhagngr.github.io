059d45052833a986867237b08872ff0e67e3b57a
talhagngr <111577996+talhagngr@users.noreply.github.com>
2023-12-16 09:57:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById('spotifyLoginButton').addEventListener('click', function() {
-  window.location.href = 'YOUR_SPOTIFY_AUTHORIZATION_URL';
+  window.location.href = 'https://accounts.spotify.com/authorize?client_id=6aaaeb6d3a884d5d94bf46bcdab165e1&response_type=code&redirect_uri=https://talhagngr.github.io/&scope=1
+; 
 });
 
