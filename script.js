45671b7591c7abd05af91778571db3a6d562d726
talhagngr <111577996+talhagngr@users.noreply.github.com>
2023-12-16 10:03:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('spotifyLoginButton').addEventListener('click', function() {
-  window.location.href = 'https://accounts.spotify.com/authorize?client_id=6aaaeb6d3a884d5d94bf46bcdab165e1&response_type=code&redirect_uri=https://talhagngr.github.io/&scope=1
+  window.location.href = 'https://accounts.spotify.com/authorize?client_id=6aaaeb6d3a884d5d94bf46bcdab165e1&response_type=code&redirect_uri=https://talhagngr.github.io/&scope=user-library-read%20playlist-read-private%20playlist-read-collaborative
 ; 
 });
 
