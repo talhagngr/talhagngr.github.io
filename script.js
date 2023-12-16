6f9c82e343c02c98ea2575f41c6563e82c9c4168
talhagngr <111577996+talhagngr@users.noreply.github.com>
2023-12-16 21:37:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementById('spotifyLoginButton').addEventListener('click', function() {
   window.location.href = 'https://accounts.spotify.com/authorize?client_id=6aaaeb6d3a884d5d94bf46bcdab165e1&response_type=code&redirect_uri=https://talhagngr.github.io/&scope=user-library-read%20playlist-read-private%20playlist-read-collaborative';
 });
+
+function fetchUserPlaylists(accessToken) {
+    return fetch('https://api.spotify.com/v1/me/playlists', {
+        headers: { 'Authorization': 'Bearer ' + accessToken }
+    })
+    .then(response => response.json())
+    .then(data => data.items); // 'items' will contain the playlists
+}
