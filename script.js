a508cb42ad4ba3425c3ebc24b17e3742c54c0006
talhagngr <111577996+talhagngr@users.noreply.github.com>
2023-12-17 10:03:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,40 @@ function getAuthorizationCode() {
     const authCode = urlParams.get('code');
     if (authCode) {
         console.log("Authorization Code:", authCode);
-        // You can now use the authCode for further processing
+        exchangeAuthCodeForToken(authCode); // Exchange the authCode for an access token
     }
+}
+
+// Function to exchange the authorization code for an access token
+function exchangeAuthCodeForToken(authCode) {
+    // Your client ID and client secret
+    const clientId = '6aaaeb6d3a884d5d94bf46bcdab165e1';
+    const clientSecret = 'bee9e6ab487d4a3aa70fc310e61fc950';
+
+    // Prepare the request body
+    const body = new URLSearchParams();
+    body.append('grant_type', 'authorization_code');
+    body.append('code', authCode);
+    body.append('redirect_uri', 'https://talhagngr.github.io/');
+    body.append('client_id', clientId);
+    body.append('client_secret', clientSecret);
+
+    // Make the request to the Spotify Accounts service
+    fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: body
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Access Token:', data.access_token);
+        // Use the access token to fetch user playlists
+        fetchUserPlaylists(data.access_token).then(playlists => {
+            console.log('User Playlists:', playlists);
+            // Further processing or display of playlists
+        });
+    })
+    .catch(error => console.error('Error:', error));
 }
 
 window.onload = getAuthorizationCode; // Call the function when the page loads
