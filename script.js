1bf962e11998e2313c7e3797509f01b6cb439dd3
talhagngr <111577996+talhagngr@users.noreply.github.com>
2023-12-17 10:24:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,17 @@ function fetchUserPlaylists(accessToken) {
     return fetch('https://api.spotify.com/v1/me/playlists', {
         headers: { 'Authorization': 'Bearer ' + accessToken }
     })
-    .then(response => response.json())
-    .then(data => data.items); // 'items' will contain the playlists
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('Playlists fetched successfully', data);
+        displayPlaylists(data.items); // Assuming you have a displayPlaylists function
+    })
+    .catch(error => console.error('Error fetching playlists:', error));
 }
 
 function getAuthorizationCode() {
@@ -16,40 +25,37 @@ function getAuthorizationCode() {
     const authCode = urlParams.get('code');
     if (authCode) {
         console.log("Authorization Code:", authCode);
-        exchangeAuthCodeForToken(authCode); // Exchange the authCode for an access token
+        exchangeAuthCodeForToken(authCode);
+    } else {
+        console.log("No authorization code found in URL.");
     }
 }
 
-// Function to exchange the authorization code for an access token
 function exchangeAuthCodeForToken(authCode) {
-    // Your client ID and client secret
     const clientId = '6aaaeb6d3a884d5d94bf46bcdab165e1';
-    const clientSecret = 'bee9e6ab487d4a3aa70fc310e61fc950';
+    const clientSecret = 'YOUR_CLIENT_SECRET'; // Use a secure method to store and retrieve your client secret
 
-    // Prepare the request body
     const body = new URLSearchParams();
     body.append('grant_type', 'authorization_code');
     body.append('code', authCode);
     body.append('redirect_uri', 'https://talhagngr.github.io/');
-    body.append('client_id', clientId);
-    body.append('client_secret', clientSecret);
 
-    // Make the request to the Spotify Accounts service
     fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: body
     })
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok during token exchange');
+        }
+        return response.json();
+    })
     .then(data => {
         console.log('Access Token:', data.access_token);
-        // Use the access token to fetch user playlists
-        fetchUserPlaylists(data.access_token).then(playlists => {
-            console.log('User Playlists:', playlists);
-            // Further processing or display of playlists
-        });
+        return fetchUserPlaylists(data.access_token);
     })
-    .catch(error => console.error('Error:', error));
+    .catch(error => console.error('Error during token exchange:', error));
 }
 
-window.onload = getAuthorizationCode; // Call the function when the page loads
+window.onload = getAuthorizationCode;
